a5d4959e68c05218ac3db0e99fa9b3b9fb4852db
Ibnu Halim Mustofa <ibnuhalim@pm.me>
2022-06-03 07:51:28 +0700

    redirect /dashboard to /

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'verified'])->group( function () {
+    Route::get('/dashboard', function () {
+        return redirect()->route('dashboard');
+    });
+
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::get('/expense', function() {
