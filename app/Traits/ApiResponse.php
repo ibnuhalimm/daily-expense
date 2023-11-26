e4934d2b4d79ce07353f9162b0c9c697dc70ea6a
Ibnu Halim Mustofa <ibnuhalim@pm.me>
2023-11-26 16:31:13 +0700

    fix parameter for apiResponse

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -3,7 +3,7 @@
 namespace App\Traits;
 
 trait ApiResponse {
-    function apiResponse($statusCode = 200, $message, $data = []) {
+    function apiResponse($statusCode, $message, $data = []) {
         return response()->json([
             'message' => $message,
             'data' => $data
